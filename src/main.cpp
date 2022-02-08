6c5142e2b7228a3a2ad1ed315611b21a04f776ac
Paul Foley <thepaulfoley@gmail.com>
2022-02-07 20:58:56 -0500

    Runs with MPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include "Msg.pb.h"
 #include "mpi.h"
 
-int main (int argc, char** argv){
+const int RECEIVER_RANK = 0;
+
+void runAsReceiver(const int& rank )
+{
+  std::cout<<__func__<<":"<<__LINE__<<" I am a receiver"<< " | rank "<<rank<<std::endl;
+}
+
+void runAsSender(const int& rank)
+{
+  std::cout<<__func__<<":"<<__LINE__<<" I am a sender"<< " | rank "<<rank<<std::endl;
   containerizedapp::ping msg;
+}
+
+int main (int argc, char** argv){
+  MPI_Init(&argc, &argv);
+  int rank;
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  if (rank == RECEIVER_RANK)
+  {
+    runAsReceiver(rank);
+  }
+  else
+  {
+    runAsSender(rank);
+  }
+
+  MPI_Finalize();
 
   return 0;
 }
